82a056c1b1f7bf137a9c6cae7bffe2e33fd9f7d3
Nikhil Bansal <48425495+Nikhilbansal777@users.noreply.github.com>
2024-06-24 16:17:57 +0530

    added react assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
 
+import React, { useEffect, useState } from 'react';
+import "../src/style.css"
+const printStateMent = async () => {
+  var images = [];
+  let res = await fetch('https://dog.ceo/api/breeds/list/all');
+  let data = await res.json();
+
+  let keys = Object.keys(data.message);
+
+  let fetchPromises = keys.map((name) => {
+    return fetch(`https://dog.ceo/api/breed/${name}/images/random`).then((res) => res.json());
+  });
+
+  let imageResults = await Promise.all(fetchPromises);
+  console.log(imageResults);
+
+  imageResults.forEach((result) => {
+    images.push(result.message);
+  });
+  return images;
+};
 function App() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      const result = await printStateMent();
+      setImages(result);
+    };
+    fetchImages();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {images.map((image) => {
+        return (
+            <img src={image} />
+        );
+      })}
     </div>
   );
 }
